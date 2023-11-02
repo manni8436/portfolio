2e8af99e483b48c64be3bdc88d3218add0b1ca93
Emanuel Silva <emanuelsilva019@gamil.com>
2023-11-02 19:58:01 +0000

    Created routes for each navigation page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  root 'pages#welcome'
+  root 'pages#home'
+  get 'projects' => 'pages#projects'
+  get 'tech' => 'pages#tech'
+  get 'work' => 'pages#work'
+  get 'contact' => 'pages#contact'
 end
